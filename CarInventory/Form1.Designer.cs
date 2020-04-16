f9a1694f997cca7e8e90e88e10fbc5af05082316
Declan k <decktehboss@hotmail.com>
2020-04-16 15:37:18 -0400

    worked on

--- CarInventory/Form1.Designer.cs
+++ CarInventory/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // yearInput
@@ -123,11 +124,22 @@
             this.label4.TabIndex = 9;
             this.label4.Text = "Mileage";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(379, 366);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -139,7 +151,7 @@
             this.Controls.Add(this.makeInput);
             this.Controls.Add(this.yearInput);
             this.Name = "Form1";
-            this.Text = "Car Inventory";
+            this.Text = "qq";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,6 +169,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CarInventory/Form1.cs
+++ CarInventory/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace CarInventory
 {
@@ -16,8 +17,9 @@ namespace CarInventory
         public Form1()
         {
             InitializeComponent();
+            loadDB();
         }
-
+       
         private void addButton_Click(object sender, EventArgs e)
         {
             string year, make, colour, mileage;
@@ -30,13 +32,14 @@ namespace CarInventory
             Car car = new Car(year, make, colour, mileage);
 
             carsList.Add(car);
-
+             
             outputLabel.Text = "";
 
-            //for (int i = 0; i < carsList.Count; i++)
-            //{
-            //    outputLabel.Text += carsList[i].year + " " + carsList[i].make + " " + carsList[i].colour + " " + carsList[i].mileage + "\n";
-            //}
+
+            for (int i = 0; i < carsList.Count; i++)
+            {
+                outputLabel.Text += carsList[i].year + " " + carsList[i].make + " " + carsList[i].colour + " " + carsList[i].mileage + "\n";
+            }
 
             foreach(Car c in carsList)
             {
@@ -45,6 +48,69 @@ namespace CarInventory
                     + c.colour + " "
                     + c.mileage + "\n";
             }
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveDB();
+            Application.Exit();
+        }
+         public void saveDB()
+        {
+            XmlWriter writer = XmlWriter.Create("Resources/CarXML.xml");
+
+            writer.WriteStartElement("cars");
+
+            foreach (Car c in carsList)
+            {
+                writer.WriteStartElement("car");
+
+                // writer.WriteElementString("id", Convert.ToString(e.id));
+                writer.WriteElementString("year", c.year);
+                writer.WriteElementString("make", c.make);
+                writer.WriteElementString("colour", c.colour);
+                writer.WriteElementString("mileage", c.mileage);
+
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+
+            writer.Close();
+        }
+        public void loadDB()
+        {
+            //int newID;
+            string newYr, newMk, newCl, newMl;
+
+            XmlReader reader = XmlReader.Create("Resources/CarXML.xml");
+
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Text)
+                {
+                    // newID = Convert.ToInt16(reader.ReadString());
+
+                    reader.ReadToNextSibling("year");
+                    newYr = reader.ReadString();
+
+                    reader.ReadToNextSibling("make");
+                    newMk = reader.ReadString();
+
+                    reader.ReadToNextSibling("colour");
+                    newCl = reader.ReadString();
+
+                    reader.ReadToNextSibling("mileage");
+                    newMl = reader.ReadString();
+
+                    Car c = new Car(newYr, newMk, newCl, newMl);
+                    carsList.Add(c);
+                }
+            }
+
+            reader.Close();
         }
     }
 }
