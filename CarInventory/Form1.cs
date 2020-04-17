2e98ba9c7041f039c4818088cece867e1323afe8
Declan k <decktehboss@hotmail.com>
2020-04-17 19:03:29 -0400

    Fixed and final

--- CarInventory/Form1.cs
+++ CarInventory/Form1.cs
@@ -18,6 +18,7 @@ namespace CarInventory
         {
             InitializeComponent();
             loadDB();
+            display();
         }
        
         private void addButton_Click(object sender, EventArgs e)
@@ -33,21 +34,8 @@ namespace CarInventory
 
             carsList.Add(car);
              
-            outputLabel.Text = "";
-
-
-            for (int i = 0; i < carsList.Count; i++)
-            {
-                outputLabel.Text += carsList[i].year + " " + carsList[i].make + " " + carsList[i].colour + " " + carsList[i].mileage + "\n";
-            }
-
-            foreach(Car c in carsList)
-            {
-                outputLabel.Text += c.year + " "
-                    + c.make + " "
-                    + c.colour + " "
-                    + c.mileage + "\n";
-            }
+            
+           display();
 
            
         }
@@ -80,6 +68,18 @@ namespace CarInventory
 
             writer.Close();
         }
+        public void display()
+        {
+            outputLabel.Text = "";
+            foreach (Car c in carsList)
+            {
+                outputLabel.Text += c.year + " "
+                    + c.make + " "
+                    + c.colour + " "
+                    + c.mileage + "\n";
+            }
+
+        }
         public void loadDB()
         {
             //int newID;
@@ -93,7 +93,7 @@ namespace CarInventory
                 {
                     // newID = Convert.ToInt16(reader.ReadString());
 
-                    reader.ReadToNextSibling("year");
+                   // reader.ReadToNextSibling("year");
                     newYr = reader.ReadString();
 
                     reader.ReadToNextSibling("make");
